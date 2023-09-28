56238bc7f243ecdedd1e963d42c16e6e234e2a3e
ephyran <sjohnson1149@gmail.com>
2023-09-28 10:58:19 -0400

    Add framework

---
+++ src/main/java/com/f23/shoppeasy/ShoppeasyApplication.java
@@ -0,0 +1,13 @@
+package com.f23.shoppeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f23/shoppeasy/ShoppeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f23.shoppeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
