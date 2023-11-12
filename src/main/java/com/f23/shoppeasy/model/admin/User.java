f67466b27c972dbdc7d31b27bc4fdd11a5a3938b
Dragon Phiansin <142546629+DragonPhia@users.noreply.github.com>
2023-11-12 02:38:09 -0500

    administration "delete user"

---
+++ src/main/java/com/f23/shoppeasy/model/admin/User.java
@@ -0,0 +1,38 @@
+package com.f23.shoppeasy.model.admin;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author drago
+ */
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String userName;
+    private String email;
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/f23/shoppeasy/model/admin/UserController.java
@@ -0,0 +1,64 @@
+package com.f23.shoppeasy.model.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author drago
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/all")
+    public String getAllUsers(Model model) {
+        model.addAttribute("userList", service.getAllUsers());
+        return "user/list-user";
+    }
+
+    @GetMapping("/id={id}")
+    public String getUser(@PathVariable long id, Model model) {
+        model.addAttribute("user", service.getUser(id));
+        return "user/user-detail";
+    }
+
+    @GetMapping("/delete/id={id}")
+    public String deleteUser(@PathVariable long id, Model model) {
+        service.deleteUser(id);
+        return "redirect:/user/all";
+    }
+
+    @PostMapping("/create")
+    public String createUser(User user) {
+
+        service.saveUser(user);
+        return "redirect:/user/all";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(User user) {
+        service.updateUser(user);
+        return "redirect:/user/all";
+    }
+
+    @GetMapping("/new-user")
+    public String newUserForm(Model model) {
+        return "user/new-user";
+    }
+
+    @GetMapping("/update/id={id}")
+    public String updateUserForm(@PathVariable long id, Model model) {
+        model.addAttribute("user", service.getUser(id));
+        return "user/update-user";
+    }
+
+}
