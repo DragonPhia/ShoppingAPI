055f6f9523b523ea6071624b1ff290fbb119ac03
Jeong Kim <113068472+Pixris@users.noreply.github.com>
2023-11-16 01:59:26 -0500

    Update CartController.java

--- src/main/java/com/f23/shoppeasy/cart/CartController.java
+++ src/main/java/com/f23/shoppeasy/cart/CartController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/cart")
@@ -18,17 +20,30 @@ public class CartController {
     public String getAllProducts(Model model) {
         model.addAttribute("productList",
                 service.getAllProducts());
+
         return "cart/cart";
     }
 
     @GetMapping("/add-to-cart")
     public String addItemToCart(Model model) {
-        return "cart/add-to-cart";
+        return "cart/cart";
     }
 
-    @GetMapping("/delete/{productId}")
-    public String deleteProduct(@PathVariable long productId, Model model) {
-        service.deleteProduct(productId);
-        return "redirect:/cart/list";
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable long id, Model model) {
+        service.deleteProduct(id);
+        return "cart/cart";
     }
+    
+    @PostMapping("/clear-cart")
+    public String clearCart(Model model) {
+        service.clearCart();
+        return "cart/cart";
+    }
+    
+//    @PostMapping("/update/{id}")
+//    public String updateCart(@PathVariable long id, @RequestParam("quantity") int quantity, Model model) {
+//        service.updateCart(id, quantity);
+//        return "cart/cart";
+//    }
 }
