658b46f07e675d567373ddb23ee73ca66d770d3c
Dragon Phiansin <142546629+DragonPhia@users.noreply.github.com>
2023-11-25 00:39:54 -0500

    Update User.java

--- src/main/java/com/f23/shoppeasy/model/admin/User.java
+++ src/main/java/com/f23/shoppeasy/model/admin/User.java
@@ -30,10 +30,14 @@ public class User {
     private String firstname;
     private String lastname;
     private String email;
+    private String role;
+    private String password;
 
-    public User(String firstname, String lastname, String email) {
+    public User(String firstname, String lastname, String email, String role, String password) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
+        this.role = role;
+        this.password = password;
     }
 }
