b4d06f52c410e1d118c48b618018c72856ab7de6
Dragon Phiansin <142546629+DragonPhia@users.noreply.github.com>
2023-12-01 21:55:17 -0500

    added security

---
+++ src/main/java/com/f23/shoppeasy/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.f23.shoppeasy.security;
+
+import com.f23.shoppeasy.model.admin.CustomUserDetailsService;
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+/**
+ *
+ * @author sentini
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName(null);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                        DispatcherType.ERROR).permitAll()
+                //.requestMatchers("/listings/**").hasAuthority("MOD")
+                //.requestMatchers("/user/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                .loginPage("/login")
+                .permitAll()
+                ).exceptionHandling((x) -> x.accessDeniedPage("/403"))
+                .logout((logout) -> logout.permitAll())
+                .requestCache((cache) -> cache
+                .requestCache(requestCache)
+                );
+
+        return http.build();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(
+                passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
