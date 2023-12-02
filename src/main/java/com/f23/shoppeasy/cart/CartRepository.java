24aaf982d84e246e3560a36ec4f5fb2ed9e8995f
Jeong Kim <113068472+Pixris@users.noreply.github.com>
2023-12-02 01:42:28 -0500

    Update CartRepository.java

--- src/main/java/com/f23/shoppeasy/cart/CartRepository.java
+++ src/main/java/com/f23/shoppeasy/cart/CartRepository.java
@@ -7,16 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartEntry, Long> {
-    /*
-    List<CartEntry> findByName(String name);
 
-    @Query("SELECT c FROM Cart c WHERE CONCAT(c.name, c.type) LIKE %?1%")
-    List<CartEntry> search(String keyword);
-    
-    @Override
-    public List<CartEntry> findAll();
-*/
-    
     @Query("SELECT c FROM CartEntry c WHERE c.userId = ?1")
     public List<CartEntry> findAll(long userId);
    
